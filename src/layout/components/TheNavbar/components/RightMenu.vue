088450afc42b134c24b0ed849c2bfdd55373f6c0
chuhx <chuhx1024@126.com>
2021-01-05 15:20:52 +0800

    Feat: Add logout

--- src/layout/components/TheNavbar/components/RightMenu.vue
+++ src/layout/components/TheNavbar/components/RightMenu.vue
@@ -2,13 +2,13 @@
     <div class="RightMenu-container">
         <a-dropdown>
             <a class="ant-dropdown-link" @click="e => e.preventDefault()">
-                欢迎 <a-icon type="down" />
+                欢迎{{username}} <a-icon type="down" />
             </a>
             <a-menu slot="overlay">
                 <a-menu-item>
                     个人中心
                 </a-menu-item>
-                <a-menu-item>
+                <a-menu-item @click="logout">
                     退出
                 </a-menu-item>
             </a-menu>
@@ -17,8 +17,19 @@
 </template>
 
 <script>
+import { removeToken } from '@/utils/handleCookie'
+import { mapState } from 'vuex'
 export default {
     name: 'RightMenu',
+    computed: {
+        ...mapState('user', ['username']),
+    },
+    methods: {
+        logout () {
+            removeToken()
+            this.$router.push('/login')
+        },
+    },
 
 }
 </script>
